a9878b16f2e657ca5451bb94d37bfcb5e285739e
yuri2397 <diawlemor97@gmail.com>
2021-10-29 22:29:17 +0000

    Deploiement

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,7 +38,6 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \Illuminate\Session\Middleware\AuthenticateSession::class,
-            \App\Http\Middleware\HttpsProtocol::class,
         ],
 
         'api' => [
